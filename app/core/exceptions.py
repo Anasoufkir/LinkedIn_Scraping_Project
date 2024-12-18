6facd44838769cd39311ddaa550f539983cad57f
oufkiranas <anasoufkir94@gmail.com>
2024-12-18 21:12:05 +0100

    [INIT] initaliser les exceptions

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -1,5 +1,15 @@
+from fastapi import HTTPException
+from starlette.status import HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_429_TOO_MANY_REQUESTS
 
-class CustomException(Exception):
-    def __init__(self, name: str, message: str):
-        self.name = name
-        self.message = message
+class ScrapingRateLimitException(HTTPException):
+    def __init__(self, detail: str = "Rate limit exceeded. Please wait before making more requests."):
+        super().__init__(status_code=HTTP_429_TOO_MANY_REQUESTS, detail=detail)
+
+class ResourceNotFoundException(HTTPException):
+    def __init__(self, resource: str):
+        detail = f"{resource} not found."
+        super().__init__(status_code=HTTP_404_NOT_FOUND, detail=detail)
+
+class BadRequestException(HTTPException):
+    def __init__(self, detail: str = "Invalid request parameters."):
+        super().__init__(status_code=HTTP_400_BAD_REQUEST, detail=detail)
