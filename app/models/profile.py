d108d19f6dcba71aed7bb36ed7c578a143483e4c
oufkiranas <anasoufkir94@gmail.com>
2024-12-18 21:15:09 +0100

    [INIT] initaliser le modele profile

--- app/models/profile.py
+++ app/models/profile.py
@@ -1,10 +1,16 @@
-
-from sqlalchemy import Column, Integer, String
-from ..core.database import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from app.core.database import Base
 
 class Profile(Base):
     __tablename__ = "profiles"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    title = Column(String)
+    name = Column(String, nullable=False)
+    current_position = Column(String, nullable=False)
+    skills = Column(String, nullable=True)  # Stocke les compétences sous forme de texte séparé par des virgules
+    experience = Column(String, nullable=True)  # Expériences passées
+
+    # Relation avec les entreprises si nécessaire
+    company_id = Column(Integer, ForeignKey("companies.id"))
+    company = relationship("Company", back_populates="employees")
