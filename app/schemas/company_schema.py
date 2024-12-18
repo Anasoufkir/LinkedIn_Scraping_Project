21f70495171ee718d20700b19af498cfeb1ddaed
oufkiranas <anasoufkir94@gmail.com>
2024-12-18 21:23:02 +0100

    [INIT] initaliser le schemas companies

--- app/schemas/company_schema.py
+++ app/schemas/company_schema.py
@@ -1,6 +1,24 @@
-
 from pydantic import BaseModel
+from typing import Optional
 
-class CompanySchema(BaseModel):
+class CompanyCreate(BaseModel):
     name: str
-    industry: str
+    sector: str
+    location: Optional[str] = None
+    employees_count: Optional[int] = None
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "TechCorp",
+                "sector": "Information Technology",
+                "location": "New York",
+                "employees_count": 250
+            }
+        }
+
+class CompanyResponse(CompanyCreate):
+    id: int  # Identifiant de l'entreprise
+
+    class Config:
+        orm_mode = True
