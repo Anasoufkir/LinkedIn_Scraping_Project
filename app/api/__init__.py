f9fd2feb79c3b1632836c8f92aad234963109baf
oufkiranas <anasoufkir94@gmail.com>
2024-12-18 20:51:42 +0100

    [INIT] intialisation de l'api

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,1 +1,9 @@
-# Fichier d'initialisation pour le module API
+from fastapi import APIRouter
+from app.api import profiles, companies, jobs
+
+router = APIRouter()
+
+# Enregistrement des routes pour chaque entité
+router.include_router(profiles.router, prefix="/profiles", tags=["Profiles"])
+router.include_router(companies.router, prefix="/companies", tags=["Companies"])
+router.include_router(jobs.router, prefix="/jobs", tags=["Jobs"])
