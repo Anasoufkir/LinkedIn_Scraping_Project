4cef6681a6a706a0f679b4f4464530f9e18a0092
oufkiranas <anasoufkir94@gmail.com>
2024-12-18 21:20:48 +0100

    [INIT] initaliser le schemas init

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,1 +1,4 @@
-# Fichier d'initialisation pour les schémas de validation
+# Initialisation du module schemas
+from .profile_schema import ProfileCreate, ProfileResponse
+from .company_schema import CompanyCreate, CompanyResponse
+from .job_schema import JobCreate, JobResponse
