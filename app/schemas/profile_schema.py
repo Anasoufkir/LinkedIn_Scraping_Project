09844f2a72fb516e60cdb0c7b65ced0c4ccd4043
oufkiranas <anasoufkir94@gmail.com>
2024-12-18 21:22:14 +0100

    [INIT] initaliser le schemas profiles

--- app/schemas/profile_schema.py
+++ app/schemas/profile_schema.py
@@ -1,6 +1,24 @@
-
 from pydantic import BaseModel
+from typing import List, Optional
 
-class ProfileSchema(BaseModel):
+class ProfileCreate(BaseModel):
     name: str
-    title: str
+    current_position: str
+    skills: Optional[List[str]] = None  # Liste des compétences
+    experience: Optional[List[str]] = None  # Liste des expériences passées
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "John Doe",
+                "current_position": "Software Engineer",
+                "skills": ["Python", "FastAPI", "SQL"],
+                "experience": ["Company A", "Company B"]
+            }
+        }
+
+class ProfileResponse(ProfileCreate):
+    id: int  # Identifiant du profil
+
+    class Config:
+        orm_mode = True
