c07c1daf20f2617969e361f57040bbf2e0d35921
oufkiranas <anasoufkir94@gmail.com>
2024-12-18 21:15:30 +0100

    [INIT] initaliser le modele companie

--- app/models/company.py
+++ app/models/company.py
@@ -1,10 +1,15 @@
-
 from sqlalchemy import Column, Integer, String
-from ..core.database import Base
+from sqlalchemy.orm import relationship
+from app.core.database import Base
 
 class Company(Base):
     __tablename__ = "companies"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    industry = Column(String)
+    name = Column(String, nullable=False, unique=True)
+    sector = Column(String, nullable=False)
+    location = Column(String, nullable=True)
+    employees_count = Column(Integer, nullable=True)
+
+    # Relation avec les employés (profils)
+    employees = relationship("Profile", back_populates="company")
