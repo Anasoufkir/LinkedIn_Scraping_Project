dc2bd4bd7fdf5d86838c788f6cdda168059efde6
oufkiranas <anasoufkir94@gmail.com>
2024-12-18 21:15:54 +0100

    [INIT] initaliser le modele jobs

--- app/models/job.py
+++ app/models/job.py
@@ -1,10 +1,15 @@
-
-from sqlalchemy import Column, Integer, String
-from ..core.database import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from app.core.database import Base
 
 class Job(Base):
     __tablename__ = "jobs"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String)
+    title = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    location = Column(String, nullable=True)
+
+    # Relation avec l'entreprise
+    company_id = Column(Integer, ForeignKey("companies.id"))
+    company = relationship("Company", back_populates="jobs")
